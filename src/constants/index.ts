5121954b22fb731ae4f7ffd026bcbe448e9b83d5
faisalsayed10 <faisal.sayed502@gmail.com>
2021-03-30 15:40:23 +0530

    constants and helpers

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,5 @@
-export {};
+export const collatedTasks = [
+  { key: "INBOX", name: "Inbox" },
+  { key: "TODAY", name: "Today" },
+  { key: "NEXT_WK", name: "Next Week" },
+];
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,1 +1,4 @@
-export {};
+import { collatedTasks } from "../constants";
+
+export const collatedTasksExist = (selectedProject: string | number) =>
+  collatedTasks.find((task) => task.key === selectedProject);
